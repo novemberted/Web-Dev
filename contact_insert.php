acfe95b36f541a426dc5e56aca7b8608f1a7fd17
novemberted <samfengyuwang@gmail.com>
2015-12-01 09:44:00 -0800

    changes made since draft1

--- contact_insert.php
+++ contact_insert.php
@@ -1,21 +1,14 @@
 <html>
 <body>
 	<?php
-	$servername = "localhost";
-	$username = "root";
-	$password = "";
-	$dbname = "final_project";
-
-	// Create connection
-	$conn = new mysqli($servername, $username, $password, $dbname);
-	// Check connection
-	if ($conn->connect_error) {
-    		die("Connection failed: " . $conn->connect_error);
-		} 
+	include "connection.php";
 
 	$name = $_POST['name'];
 	$email = $_POST['email'];
 	$password = $_POST['password'];
+	$name = mysql_real_escape_string($name);
+	$email = mysql_real_escape_string($email);
+	$password = mysql_real_escape_string($password);
 
 	//Insert data
 	$sql = "INSERT INTO contacts(name, email, password)
@@ -28,11 +21,7 @@
 	}
 
 	$conn->close();
+	header("Location: contact_list.php");
 	?>
-
-	<div align="center">
-		<h1>Create contact Success</h1>
-		<h1><a href="http://localhost/Ted/contact_list.php">Go to contact</a></h1>
-	</div>
 </body>
 </html>
